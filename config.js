021f00362f54bcb3558b61fb899e3a396a74daaa
bugbot6666 <114159489+bugbot6666@users.noreply.github.com>
2022-12-06 16:05:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'FILHO MODS'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6285781198694']
+global.premium = ['6285781198694']
+global.packname = 'Filho Mods'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
